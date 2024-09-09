c36f426ac22e72c61d1d8a397fa4c4ccb6814446
oleksiypalamarchuck@gmail.com <oleksiypalamarchuck@gmail.com>
2024-09-09 14:28:28 +0300

    start adding makeassume command

---
+++ BelieveOrNotBelieveGameServer/Application/GameTable/Commands/MakeAssumeCommand/MakeAssumeCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Abstractions.GameAbstractions;
+using Domain.Models.GameModels;
+using MediatR;
+
+namespace Application.GameTable.Commands.MakeAssumeCommand
+{
+    public class MakeAssumeCommandHandler : IRequestHandler<MakeAssumeCommandRequest, MakeAssumeCommandResponse>
+    {
+        private readonly IGameTableService _gameTableService;
+
+        public MakeAssumeCommandHandler(IGameTableService gameTableService)
+        {
+            _gameTableService = gameTableService;
+        }
+        public Task<MakeAssumeCommandResponse> Handle(MakeAssumeCommandRequest request, CancellationToken cancellationToken)
+        {
+            Player? caller = _gameTableService.GetPlayerWithConnectionId(request.CallerConnectionId, request.GameName);
+
+            if (caller == null)
+            {
+                throw new Exception("Player with this connection id does not exist");
+            }
+
+            _gameTableService.MakeAssume(request.GameName, request.IBelieve);
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BelieveOrNotBelieveGameServer/Application/GameTable/Commands/MakeAssumeCommand/MakeAssumeCommandResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.GameTable.Commands.MakeAssumeCommand
+{
+    public class MakeAssumeCommandResponse
+    {
+        public int Result { get; set; } = 0;
+        public string Message { get; set; } = string.Empty;
+    }
+}
