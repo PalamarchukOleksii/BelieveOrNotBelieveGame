73421a629136d10a72f7f48c938af842395800cd
Oleksii Palamarchuk <144825066+PalamarchukOleksii@users.noreply.github.com>
2024-10-30 02:52:42 +0200

    add GetGameTableByConnectionId method

--- BelieveOrNotBelieveGameServer/Domain/Abstractions/GameAbstractions/IGameTableService.cs
+++ BelieveOrNotBelieveGameServer/Domain/Abstractions/GameAbstractions/IGameTableService.cs
@@ -8,16 +8,6 @@ namespace Domain.Abstractions.GameAbstractions
         public GameTable? CreateGameTable(GameTableOptions gameTableOptions);
         public GameTable? GetGameTableByName(string gameName);
         public bool DeleteGameTableByName(string gameName);
-        //public bool CreateGame(GameTableOptions options);
-        //public string JoinGame(string gameName, string username, string connectionId);
-        //public StartGameResponse StartGame(string gameName, Player player);
-        //public MakeMoveResponse MakeMove(string gameName, Player player, string cardsValue, string cardsId);
-        //public void MakeAssume(string gameName, bool iBelieve);
-        //public void EndGame(string gameName);
-        //Player? GetPlayerWithConnectionId(string callerConnectionId, string gameName);
-        //GetInfoAboutOpponentsResponse GetInfoAboutOpponents(string gameName);
-        //GetPlayersCardsResponse GetPlayersCards(string gameName);
-        //List<string> GetPlayersConnectionIds(string gameName);
-        //List<string> GetPlayersWhoWinConnectionIds(string gameName);
+        public GameTable? GetGameTableByConnectionId(string connectionId);
     }
 }
--- BelieveOrNotBelieveGameServer/Domain/Services/GameServices/GameTableService.cs
+++ BelieveOrNotBelieveGameServer/Domain/Services/GameServices/GameTableService.cs
@@ -46,100 +46,16 @@ namespace Domain.Services.GameServices
             return table;
         }
 
-        //public void EndGame(string gameName)
-        //{
-        //    GameTable? table = gameTables.Find(x => x.Options.GameName == gameName);
+        public GameTable? GetGameTableByConnectionId(string connectionId)
+        {
+            GameTable? table = gameTables.Find(x => x.Players.Exists(y => y.PlayerConnectionId == connectionId));
 
-        //    if (table == null)
-        //    {
-        //        throw new Exception("Table with that name does not exist");
-        //    }
+            if (table is null)
+            {
+                return default;
+            }
 
-        //    table.EndGame();
-        //}
-
-        //public GetInfoAboutOpponentsResponse GetInfoAboutOpponents(string gameName)
-        //{
-        //    GameTable? table = gameTables.Find(x => x.Options.GameName == gameName);
-
-        //    if (table == null)
-        //    {
-        //        throw new Exception("Table with that name does not exist");
-        //    }
-
-        //    return new GetInfoAboutOpponentsResponse
-        //    {
-        //        Success = true,
-        //        OpponentInfo = table.Players
-        //        .Select(p => new OpponentInfoDto
-        //        {
-        //            PlayerConnectionId = p.PlayerConnectionId,
-        //            Name = p.Name,
-        //            CardCount = p.PlayersCards.Count
-        //        }).ToList()
-        //    };
-        //}
-
-        //public GetPlayersCardsResponse GetPlayersCards(string gameName)
-        //{
-        //    GameTable? table = gameTables.Find(x => x.Options.GameName == gameName);
-
-        //    if (table == null)
-        //    {
-        //        throw new Exception("Table with that name does not exist");
-        //    }
-
-        //    return new GetPlayersCardsResponse
-        //    {
-        //        Success = true,
-        //        PlayersCards = table.Players
-        //        .Select(p => new PlayerCardsDto
-        //        {
-        //            PlayerConnectionId = p.PlayerConnectionId,
-        //            Cards = p.PlayersCards
-        //        }).ToList()
-        //    };
-        //}
-
-        //public List<string> GetPlayersConnectionIds(string gameName)
-        //{
-        //    GameTable? table = gameTables.Find(x => x.Options.GameName == gameName);
-
-        //    if (table == null)
-        //    {
-        //        throw new Exception("Table with that name does not exist");
-        //    }
-
-        //    return new List<string>(table.Players.Select(P => P.PlayerConnectionId));
-        //}
-
-        //public List<string> GetPlayersWhoWinConnectionIds(string gameName)
-        //{
-        //    GameTable? table = gameTables.Find(x => x.Options.GameName == gameName);
-
-        //    if (table == null)
-        //    {
-        //        throw new Exception("Table with that name does not exist");
-        //    }
-
-        //    return new List<string>(table.PlayersWhoWin.Select(P => P.PlayerConnectionId));
-        //}
-
-        //public Player? GetPlayerWithConnectionId(string callerConnectionId, string gameName)
-        //{
-        //    return gameTables.Single(x => x.Options.GameName == gameName).Players.Find(x => x.PlayerConnectionId == callerConnectionId);
-        //}
-
-        //public void MakeAssume(string gameName, bool iBelieve)
-        //{
-        //    GameTable? table = gameTables.Find(x => x.Options.GameName == gameName);
-
-        //    if (table == null)
-        //    {
-        //        throw new Exception("Table with that name does not exist");
-        //    }
-
-        //    table.MakeAssume(iBelieve);
-        //}
+            return table;
+        }
     }
 }
